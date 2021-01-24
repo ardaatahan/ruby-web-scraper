a79cc4a43dd8754cb473a6d4c5f563c0600ccb03
ardaatahan <63500745+ardaatahan@users.noreply.github.com>
2021-01-24 21:55:02 +0300

    implement scrape function

--- scrape.rb
+++ scrape.rb
@@ -31,16 +31,51 @@ def scrape driver
     
     driver.navigate.to(employees_url)
 
+    # Parse the first employee page
+    parsed_employee_page = Nokogiri::HTML(driver.page_source)
+    
+    # 
+    employee_listings = parsed_employee_page.css(".entity-result")
+    employee_per_page = employee_listings.size
+    total_employee_count = parsed_employee_page.css(".pb2.t-black--light.t-14")
+        .text.split(" ")[0].to_i
 
+    last_page = (total_employee_count.to_f / employee_per_page.to_f).ceil
+
+    current_page = 1
+    employees = Array.new
+
+    while current_page <= last_page
+        
+        if current_page != 1
+            driver.navigate.to(employees_url + "&page=#{current_page}")
+            parsed_employee_page = Nokogiri::HTML(driver.page_source)
+            employee_listings = parsed_employee_page.css(".entity-result")
+        end
+
+        employee_listings.each do |employee_listing|
+            employee = {
+                person: employee_listing.css("a").text.strip.split(/View|&|\+/).first,
+                company: parsed_company_page.css(".org-top-card-summary__title").text.strip,
+                title: employee_listing.css(".entity-result__primary-subtitle").text.strip,
+                location: employee_listing.css(".entity-result__secondary-subtitle").text.strip,
+                url: employee_listing.css(".app-aware-link")[0].attributes["href"].value,
+            }
+
+            employees << employee
+        end
+
+        current_page += 1
+    end
+
+    return employees
 end
 
 # Configure driver and navigate to LinkedIn
-caps = Selenium::WebDriver::Remote::Capabilities.chrome("goog:chromeOptions" => {detach: true})
-driver = Selenium::WebDriver.for :chrome, desired_capabilities: caps
+driver = Selenium::WebDriver.for(:chrome)
 
 sign_in(driver)
-scrape(driver)
 
-driver.manage.timeouts.implicit_wait = 100
+employees = scrape(driver)
 
 driver.quit
